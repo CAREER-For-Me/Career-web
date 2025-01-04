1369984c99d2e3cdbcdb7b7c1e257b7de95601c3
박시은 <charm516@naver.com>
2025-01-04 20:11:24 +0900

    [feat] - 활동 추천 페이지 dummy data 적용

--- app/(nonAuth)/activityRecommend/page.tsx
+++ app/(nonAuth)/activityRecommend/page.tsx
@@ -4,14 +4,26 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import SelectedForm from "@/app/components/activityRecommend/SelectedForm";
+import Link from "next/link";
 
 const ActivityRecommendationPage = () => {
   const [posts, setPosts] = useState([]);
+  const [postDetail, setPostDetail] = useState([]);
+
   useEffect(() => {
     const getPostingList = async () => {
       try {
-        const res = await axios.get("http://localhost:4000/postingList");
-        setPosts(res.data);
+        await axios
+          .all([
+            axios.get("http://localhost:4000/postingList"),
+            axios.get("http://localhost:4000/detail"),
+          ])
+          .then(
+            axios.spread((res1, res2) => {
+              setPosts(res1.data);
+              setPostDetail(res2.data);
+            })
+          );
       } catch (error) {
         console.error(error, "에러가 발생했습니다.");
       }
@@ -26,17 +38,29 @@ const ActivityRecommendationPage = () => {
       <section>
         <div>
           <p>
-            나를 위한 <select /> 추천이에요🌟
+            나를 위한
+            <select>
+              <option>대외활동</option>
+            </select>
+            추천이에요🌟
           </p>
           <p>총 {posts.length}건</p>
         </div>
-        <ul>
+        <ul className="flex flex-row gap-4 flex-wrap">
           {posts.map((post, index) => (
             <li key={index}>
-              <figure>
-                <img src={post.imageUrl} alt={post.title} />
-                <figcaption>{post.title}</figcaption>
-              </figure>
+              <Link href="./">
+                <figure>
+                  <img
+                    src={post.imageUrl}
+                    alt={post.title}
+                    className=" rounded-3xl"
+                  />
+                  <figcaption className="text-wrap w-80 font-bold text-lg">
+                    {post.title}
+                  </figcaption>
+                </figure>
+              </Link>
             </li>
           ))}
         </ul>
