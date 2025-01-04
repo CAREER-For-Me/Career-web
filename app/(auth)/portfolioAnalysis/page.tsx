3e0ffee12b3f088d97d0bc1ca85a5ce16b40de6f
topeanut <ljhh1648@gmail.com>
2025-01-04 20:42:24 +0900

    [add] 포트폴리오페이지세팅

--- app/(auth)/portfolioAnalysis/page.tsx
+++ app/(auth)/portfolioAnalysis/page.tsx
@@ -1,8 +1,23 @@
+"use client";
+import Button from "../../components/common/Button";
+import "tailwindcss/tailwind.css"; //전역 스타일시트 import
+import React, { useState } from "react";
 const PortfolioAnalysisPage = () => {
+  const [grade, setGrade] = useState("");
+  const [university, setUniversity] = useState("");
+  const [academicState, setAcademicState] = useState("");
+  const [majorType, setMajorType] = useState("주전공");
+  const [major, setMajor] = useState("");
+  const [totalGPA, setTotalGPA] = useState("");
+  const [majorGPA, setMajorGPA] = useState("");
+  const handleSubmit = () => {};
   return (
-    <main>
-      <h1 className="text-xl font-bold">포트폴리오 분석 페이지</h1>
-    </main>
+    <>
+      <main>
+        <h1 className="text-xl font-bold">내 포트폴리오 정보</h1>
+      </main>
+      <Button label="수정하기" onClick={handleSubmit}></Button>
+    </>
   );
 };
 
