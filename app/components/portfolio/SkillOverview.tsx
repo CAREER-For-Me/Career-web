ab3bdfd4ab8ea5f37e6dfde08e619eb55671b348
sieun <charm516@naver.com>
2025-01-14 20:12:32 +0900

    [feat] - 필수 스킬 chart.js 적용

--- app/components/portfolio/SkillOverview.tsx
+++ app/components/portfolio/SkillOverview.tsx
@@ -1,7 +1,9 @@
+import { useUserSpec } from "@/app/context/UserSpecContext";
 import BarChart from "../chart/BarChart";
 import { GoChevronRight } from "react-icons/go";
 
 const SkillOverview = () => {
+  const { myspec, setMyspec } = useUserSpec();
   return (
     <div className="bg-careerForMe-gray01 flex-1 rounded-md">
       <div className="flex justify-end items-center mr-5 mt-5 -mb-4">
@@ -9,15 +11,17 @@ const SkillOverview = () => {
         <GoChevronRight />
       </div>
 
-      <div className="ml-6">
-        <div className="border-2 border-careerForMe-main rounded-lg inline-block px-3 py-1 bg-white">
+      <div className="ml-6 relative">
+        <div className="border-2 border-careerForMe-main rounded-lg inline-block px-3 py-1 ml-4 bg-white">
           내 필수 스킬 <span className="text-careerForMe-main">6개</span>
         </div>
         <div className="flex items-center text-nowrap h-14 justify-between mx-5">
-          <div>
-            <BarChart />
+          <div className="bg-gray-light w-[18.8rem] h-[0.6rem] absolute rounded-full" />
+          <div className="absolute ">
+            <BarChart value={6} max={20} />
           </div>
-          <div>
+
+          <div className="absolute right-6">
             <span className="text-gray-dark">전체</span>
             <span className="text-careerForMe-main">20개</span>
           </div>
